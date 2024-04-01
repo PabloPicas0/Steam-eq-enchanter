8d809f9a9a6c1b218a810736a5d39ffcfa703864
PabloPicas0 <pawel.fe@gmail.com>
2024-04-01 17:27:21 +0200

    feat: set proxy route to fetch all needed info

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,23 +1,28 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+
 import { AddressInfo } from "net";
 
-const port = 3001;
+import getUserInfo from "./utils/getUserInfo.js"
+import getUserInventory from "./utils/getUserInventory.js";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", async (req, res) => {
-  const steamRequest = await fetch(
-    "http://steamcommunity.com/inventory/76561198196802546/730/2?l=english&count=5000"
-  );
-  const steamData = await steamRequest.json()
 
-  res.send(steamData);
+  const [userInfo, userInventory] = await Promise.all([
+    getUserInfo(
+      `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_KEY}&steamids=76561198196802546`
+    ),
+    getUserInventory("76561198196802546"),
+  ]);
+
+  res.send([userInfo, userInventory]);
 });
 
-const listener = app.listen(port, () => {
+const listener = app.listen(process.env.PORT || 3001, () => {
   const { port } = listener.address() as AddressInfo;
   console.log(`Your app is listening to localhost:${port}`);
   console.log("Press ctrl + c to exit");
