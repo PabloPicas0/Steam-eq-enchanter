9ecf8b940c399e5a62c20541e293d6959bca626e
PabloPicas0 <pawel.fe@gmail.com>
2024-04-02 20:27:13 +0200

    feat: create basic profile skeleton

---
+++ src/Components/ProfileSkeleton.tsx
@@ -0,0 +1,13 @@
+function ProfileSkeleton() {
+  return (
+    <main className="hero" style={{ display: "grid", placeItems: "center" }}>
+      <div className="skeleton-rounded"></div>
+      <h1 className="skeleton-rectangle"></h1>
+      <p className="skeleton-rectangle"></p>
+      <p className="skeleton-rectangle"></p>
+      <p className="skeleton-rectangle"></p>
+    </main>
+  );
+}
+
+export default ProfileSkeleton;
