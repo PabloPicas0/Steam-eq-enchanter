3c0fdf2f68a22e9aba8253c903b774d8cefc596c
PabloPicas0 <pawel.fe@gmail.com>
2024-04-02 23:15:14 +0200

    refactor: delete two rect skeletons

--- src/Components/ProfileSkeleton.tsx
+++ src/Components/ProfileSkeleton.tsx
@@ -4,8 +4,6 @@ function ProfileSkeleton() {
       <div className="skeleton-rounded"></div>
       <h1 className="skeleton-rectangle"></h1>
       <p className="skeleton-rectangle"></p>
-      <p className="skeleton-rectangle"></p>
-      <p className="skeleton-rectangle"></p>
     </main>
   );
 }
