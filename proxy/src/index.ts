c1fc9cc884305ca4123543f97f62962c8c262cf0
PabloPicas0 <pawel.fe@gmail.com>
2024-04-04 23:18:00 +0200

    feat: add types to api response and add additional info to inventory items

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -3,21 +3,38 @@ import cors from "cors";
 
 import { AddressInfo } from "net";
 
-import getUserInfo from "./utils/getUserInfo.js"
+import getUserInfo from "./utils/getUserInfo.js";
 import getUserInventory from "./utils/getUserInventory.js";
+
+import { UserInfoType } from "./models/UserInfoModel.js";
+import { UserInvenotryType } from "./models/UserInventoryModel.js";
+
 
 const app = express();
 
 app.use(cors());
 
 app.get("/", async (req, res) => {
-
-  const [userInfo, userInventory] = await Promise.all([
+  const [userInfo, userInventory]: [UserInfoType, UserInvenotryType] = await Promise.all([
     getUserInfo(
       `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_KEY}&steamids=76561198196802546`
     ),
     getUserInventory("76561198196802546"),
   ]);
+
+  for (let i = 0; i < userInventory.assets.length; ++i) {
+    const item = userInventory.assets[i];
+
+    for (let j = 0; j < userInventory.descriptions.length; ++j) {
+      const description = userInventory.descriptions[j];
+
+      if (item.classid === description.classid) {
+        item.market_name = description.market_name;
+        item.icon_url = description.icon_url;
+        break;
+      }
+    }
+  }
 
   res.send([userInfo, userInventory]);
 });
