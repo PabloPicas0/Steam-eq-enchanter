867e7e51dedc676220293501845ca9d6efd9fb6d
PabloPicas0 <pawel.fe@gmail.com>
2024-04-06 17:02:08 +0200

    feat: create basic EquipmentSkeleton component

---
+++ src/Components/EquipmentSkeleton.tsx
@@ -0,0 +1,16 @@
+function EquipmentSkeleton() {
+  return (
+    <section className="equipment-container">
+      <h2 className="skeleton-text" style={{ gridColumn: "1 / -1" }}></h2>
+      <div className="skeleton-rect"></div>
+      <div className="skeleton-rect"></div>
+      <div className="skeleton-rect"></div>
+      <div className="skeleton-rect"></div>
+      <div className="skeleton-rect"></div>
+      <div className="skeleton-rect"></div>
+    </section>
+  );
+}
+
+
+export default EquipmentSkeleton
