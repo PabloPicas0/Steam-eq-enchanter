fa625f0faa843116e048234ccce6c7a788448407
PabloPicas0 <pawel.fe@gmail.com>
2024-04-06 16:58:58 +0200

    feat: create UserInfoType model

---
+++ proxy/src/models/UserInfoModel.ts
@@ -0,0 +1,24 @@
+export type UserInfoType = {
+  response: {
+    players: {
+      steamid: string;
+      communityvisibilitystate: number;
+      profilestate: number;
+      personaname: string;
+      commentpermission: number;
+      profileurl: string;
+      avatar: string;
+      avatarmedium: string;
+      avatarfull: string;
+      avatarhash: string;
+      lastlogoff: number;
+      personastate: number;
+      realname: string;
+      primaryclanid: string;
+      timecreated: number;
+      personastateflags: number;
+      loccountrycode: string;
+      locstatecode: string;
+    }[];
+  };
+};
