c652d87e587c1243f7b6538e51018c00ff571aad
PabloPicas0 <pawel.fe@gmail.com>
2024-04-06 16:04:20 +0200

    refactor: function filters duplicate items before return

--- proxy/src/utils/getUserInventory.ts
+++ proxy/src/utils/getUserInventory.ts
@@ -1,10 +1,20 @@
+import { UserInvenotryType } from "../models/UserInventoryModel.js";
+
+// TODO: Remove all medals, pins etc.
+// Here is the link to all of them https://steamcommunity.com/sharedfiles/filedetails/?id=470081696
+// Remove it by petterns in namings 
 async function getUserInventory(id: string) {
-  const inventory = await fetch(
-    `http://steamcommunity.com/inventory/${id}/730/2?l=english&count=5000`
-  );
-  const inventoryData = await inventory.json();
+  const inventory = await fetch(`http://steamcommunity.com/inventory/${id}/730/2?l=english&count=5000`);
+  const inventoryData: UserInvenotryType = await inventory.json();
+
+  // Remove duplicates
+  const ids = inventoryData.assets.map((item) => item.classid);
+  const filteredAssets = inventoryData.assets.filter((item, idx) => !ids.includes(item.classid, idx + 1));
+
+  inventoryData.assets = filteredAssets;
+  inventoryData.total_inventory_count = filteredAssets.length;
 
   return inventoryData;
 }
 
-export default getUserInventory
+export default getUserInventory;
