9b17ebe4daf9f60053861430a4a794504497e374
PabloPicas0 <pawel.fe@gmail.com>
2024-04-08 22:27:21 +0200

    feat: add simple validation on eq failed fetch

--- proxy/src/utils/getUserInventory.ts
+++ proxy/src/utils/getUserInventory.ts
@@ -2,10 +2,16 @@ import { UserInvenotryType } from "../models/UserInventoryModel.js";
 
 // TODO: Remove all medals, pins etc.
 // Here is the link to all of them https://steamcommunity.com/sharedfiles/filedetails/?id=470081696
-// Remove it by petterns in namings 
+// Remove it by petterns in namings
+
+// TODO: add validation
 async function getUserInventory(id: string) {
   const inventory = await fetch(`http://steamcommunity.com/inventory/${id}/730/2?l=english&count=5000`);
+
+  if (!inventory.ok) throw new Error("Unable to fetch equipment");
+
   const inventoryData: UserInvenotryType = await inventory.json();
+
 
   // Remove duplicates
   const ids = inventoryData.assets.map((item) => item.classid);
