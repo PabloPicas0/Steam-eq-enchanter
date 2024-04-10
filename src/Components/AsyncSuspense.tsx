d5b4438a3dc634c62ad4735e2feb816027b1e6ac
PabloPicas0 <pawel.fe@gmail.com>
2024-04-10 22:45:34 +0200

    feat: create AsyncSuspense component

---
+++ src/Components/AsyncSuspense.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+import EquipmentSkeleton from "./EquipmentSkeleton";
+import ProfileSkeleton from "./ProfileSkeleton";
+
+function AsyncSuspense(props: { error: boolean; pending: boolean; children: ReactNode; items: Number }) {
+  const { children, error, pending, items } = props;
+
+  if (pending) {
+    return (
+      <>
+        <ProfileSkeleton />
+        <EquipmentSkeleton />
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <h1>
+        Something went wrong. You either enter wrong url or have not fully public profile. Please check your
+        url or steam privacy settings
+      </h1>
+    );
+  }
+
+  return items ? children : null;
+}
+
+export default AsyncSuspense;
