a62d4f06b7b9547f72107db70ff2a8d2a97757ec
PabloPicas0 <pawel.fe@gmail.com>
2024-04-11 23:21:44 +0200

    refactor: change function params

--- proxy/src/utils/getUserInfo.ts
+++ proxy/src/utils/getUserInfo.ts
@@ -1,7 +1,9 @@
 import { UserInfoType } from "../models/UserInfoModel.js";
 
-async function getUserInfo(userToFetch: string) {
-    const user = await fetch(userToFetch);
+async function getUserInfo(key: string, id: string) {
+    const user = await fetch(
+      `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${id}`
+    );
 
     const userData: UserInfoType = await user.json()
 
