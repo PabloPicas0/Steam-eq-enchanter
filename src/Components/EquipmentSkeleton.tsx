71e2ce9285eb67e6e91b436bb66a87fab5f4c60b
PabloPicas0 <pawel.fe@gmail.com>
2024-04-28 21:47:11 +0200

    feat: force aligned skeleton

--- src/Components/EquipmentSkeleton.tsx
+++ src/Components/EquipmentSkeleton.tsx
@@ -1,16 +1,15 @@
 function EquipmentSkeleton() {
   return (
     <section className="items-container">
-      <h2 className="skeleton-text" style={{ gridColumn: "1 / -1" }}></h2>
-      <div className="skeleton-rect"></div>
-      <div className="skeleton-rect"></div>
-      <div className="skeleton-rect"></div>
-      <div className="skeleton-rect"></div>
-      <div className="skeleton-rect"></div>
-      <div className="skeleton-rect"></div>
+      <h2 className="skeleton-text" style={{ gridColumn: "1 / -1", placeSelf: "center" }}></h2>
+      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
+      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
+      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
+      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
+      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
+      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
     </section>
   );
 }
 
-
-export default EquipmentSkeleton
+export default EquipmentSkeleton;
