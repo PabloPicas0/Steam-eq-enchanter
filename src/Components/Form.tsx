fd56378f242c862b97f43be1cd263a8ed11d157d
PabloPicas0 <pawel.fe@gmail.com>
2024-04-28 16:01:07 +0200

    refactor: improve submit validation

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -5,23 +5,36 @@ import steam from "../assets/Steam.webp";
 function Form(props: {
   setItems: React.Dispatch<React.SetStateAction<never[]>>;
   setPending: React.Dispatch<React.SetStateAction<boolean>>;
+  setError: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const { setItems, setPending } = props;
+  const { setItems, setPending, setError } = props;
 
   const [input, setInput] = useState("");
 
   async function handleSubmit(e: React.FormEvent<HTMLInputElement>) {
     e.preventDefault();
+
+    if (input === "") return;
+
     const steam64ID = input.replace(/\D/g, "");
 
     setPending(true);
     setItems([]);
 
     const proxyResponse = await fetch(`/api?id=${steam64ID}`);
+
+    if (!proxyResponse.ok) {
+      setPending(false);
+      setError(true);
+
+      return
+    }
+
     const prxyData = await proxyResponse.json();
 
     setItems(prxyData);
     setPending(false);
+    setError(false);
   }
 
   return (
