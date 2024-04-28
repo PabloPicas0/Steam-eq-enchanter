3e96bafb96b5f754267322a981993980ceac6eac
PabloPicas0 <pawel.fe@gmail.com>
2024-04-28 20:59:34 +0200

    refactor: change container class name

--- src/Components/EquipmentSkeleton.tsx
+++ src/Components/EquipmentSkeleton.tsx
@@ -1,6 +1,6 @@
 function EquipmentSkeleton() {
   return (
-    <section className="equipment-container">
+    <section className="items-container">
       <h2 className="skeleton-text" style={{ gridColumn: "1 / -1" }}></h2>
       <div className="skeleton-rect"></div>
       <div className="skeleton-rect"></div>
