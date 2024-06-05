9ac686210d3d9936c27c7d8f01e38c55779460f9
PabloPicas0 <pawel.fe@gmail.com>
2024-06-05 14:07:34 +0200

    feat: add price_history to types

--- proxy/src/models/MarketModel.ts
+++ proxy/src/models/MarketModel.ts
@@ -1,6 +1,9 @@
+import { PriceHistoryModel } from "./PriceHistoryModel.js";
+
 export type MarketModel = {
   success: boolean;
   lowest_price: string;
   volume: string;
   median_price: string;
+  price_history?:  PriceHistoryModel
 };
