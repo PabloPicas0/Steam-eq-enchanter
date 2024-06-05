e29cabfbda79bf3de45d9060cc103cfc695a4c54
PabloPicas0 <pawel.fe@gmail.com>
2024-06-05 14:10:29 +0200

    refactor: add internal typings

--- proxy/src/utils/getPriceOverview.ts
+++ proxy/src/utils/getPriceOverview.ts
@@ -1,9 +1,11 @@
+import { MarketModel } from "../models/MarketModel.js";
+
 async function getPriceOverview(market_hash_name: string) {
   const priceOverviewResponse = await fetch(
     `https://steamcommunity.com/market/priceoverview/?&appid=730&market_hash_name=${market_hash_name}`
   );
 
-  const priceOverview = await priceOverviewResponse.json();
+  const priceOverview: MarketModel = await priceOverviewResponse.json();
 
   return priceOverview;
 }
