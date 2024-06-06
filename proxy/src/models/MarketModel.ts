949ed6c010d9b5f1061cf26351b9c62c6fa29973
PabloPicas0 <pawel.fe@gmail.com>
2024-06-06 15:50:22 +0200

    refactor: change price_history type

--- proxy/src/models/MarketModel.ts
+++ proxy/src/models/MarketModel.ts
@@ -5,5 +5,5 @@ export type MarketModel = {
   lowest_price: string;
   volume: string;
   median_price: string;
-  price_history?: { status: number; data: PriceHistoryModel };
+  price_history?: PriceHistoryModel;
 };
