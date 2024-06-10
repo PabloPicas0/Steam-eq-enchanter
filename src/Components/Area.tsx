e8cf7b2ad7d1bbef56b18a16342ba0b5ed21b139
PabloPicas0 <pawel.fe@gmail.com>
2024-06-10 15:48:58 +0200

    feat: add x axis year only format

--- src/Components/Area.tsx
+++ src/Components/Area.tsx
@@ -1,4 +1,4 @@
-import * as d3 from "d3";
+import { area, scaleTime, select, scaleLinear, extent, max, axisBottom, axisLeft, scaleUtc } from "d3";
 import { useEffect, useMemo, useRef, useState } from "react";
 
 import { AreaPropsModel } from "../models/AreaPropsModel";
@@ -6,7 +6,7 @@ import { AreaPropsModel } from "../models/AreaPropsModel";
 // TODO: Add switching between time scales
 function Area({
   data,
-  width = 640,
+  width = 800,
   height = 400,
   marginTop = 20,
   marginRight = 20,
@@ -16,31 +16,30 @@ function Area({
   const [time, setTime] = useState("month");
 
   const timeScale = useMemo(() => data.map((time) => new Date(time[0])), [time]);
-  const areaData = data.map((point) => ({ date: new Date(point[0]), price: point[1] }));
+  const areaData = data.map((vector) => ({ date: new Date(vector[0]), price: vector[1] }));
 
   const gx = useRef<SVGGElement>(null);
   const gy = useRef<SVGGElement>(null);
 
-  const x = d3.scaleTime(d3.extent(timeScale, (time) => time) as Date[], [marginLeft, width - marginRight]);
-  const y = d3.scaleLinear([0, d3.max(data, (d) => d[1]) as number], [height - marginBottom, marginTop]);
+  const x = scaleUtc(extent(timeScale, (time) => time) as Date[], [marginLeft, width - marginRight]);
+  const y = scaleLinear([0, max(data, (d) => d[1]) as number], [height - marginBottom, marginTop]);
 
-  const area = d3.area<{ date: Date; price: Number }>(
+  const chartArea = area<{ date: Date; price: Number }>(
     (d) => x(d.date),
     y(0),
     (d) => y(d.price)
   );
 
-
   useEffect(() => {
     if (!gx.current) return;
 
-    void d3.select(gx.current).call(d3.axisBottom(x).ticks(7));
+    void select(gx.current).call(axisBottom(x).ticks(7, "%Y"));
   }, [gx, x]);
 
   useEffect(() => {
     if (!gy.current) return;
 
-    void d3.select(gy.current).call(d3.axisLeft(y).ticks(5));
+    void select(gy.current).call(axisLeft(y).ticks(5));
   }, [gy, y]);
 
   return (
@@ -51,9 +50,10 @@ function Area({
       style={{ maxWidth: "100%", height: "auto" }}>
       <g ref={gx} transform={`translate(0,${height - marginBottom})`}></g>
       <g ref={gy} transform={`translate(${marginLeft},0)`}></g>
-      <path fill="steelblue" stroke="currentColor" strokeWidth="1.5" d={area(areaData) as string} />
+      <path fill="steelblue" stroke="currentColor" strokeWidth="1.5" d={chartArea(areaData) as string} />
     </svg>
   );
 }
 
-export default Area;
+
+export default Area
