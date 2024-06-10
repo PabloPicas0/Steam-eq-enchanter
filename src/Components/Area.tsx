c439bb7015bff67597ba62df86761407427764e1
PabloPicas0 <pawel.fe@gmail.com>
2024-06-10 22:11:49 +0200

    feat: add better y axis tick format

--- src/Components/Area.tsx
+++ src/Components/Area.tsx
@@ -1,13 +1,4 @@
-import {
-  area,
-  select,
-  scaleLinear,
-  extent,
-  max,
-  axisBottom,
-  axisLeft,
-  scaleUtc,
-} from "d3";
+import { area, select, scaleLinear, extent, max, axisBottom, axisLeft, scaleUtc } from "d3";
 import { useEffect, useMemo, useRef, useState } from "react";
 
 import { AreaPropsModel } from "../models/AreaPropsModel";
@@ -18,9 +9,9 @@ function Area({
   width = 800,
   height = 400,
   marginTop = 20,
-  marginRight = 20,
+  marginRight = 10,
   marginBottom = 20,
-  marginLeft = 20,
+  marginLeft = 40,
 }: AreaPropsModel) {
   const [time, setTime] = useState("month");
 
@@ -42,31 +33,33 @@ function Area({
   useEffect(() => {
     if (!gx.current) return;
 
-    // This var converts each tick to year 
+    // This var converts each tick to year
     // Next is created set to remove duplicate years
     // After that we take arr length to set custom number of ticks on each axis
-    // DO NOT DELETE MIGHT BE USEFUL IN THE FUTURE 
+    // DO NOT DELETE MIGHT BE USEFUL IN THE FUTURE
     // const ticksNumber = Array.from(new Set(x.ticks().map(x.tickFormat(0, "%Y")))).length;
-
+    console.log(max(data, (d) => d[1]));
     void select(gx.current).call(axisBottom(x).ticks(7));
   }, [gx, x]);
 
   useEffect(() => {
     if (!gy.current) return;
 
-    void select(gy.current).call(axisLeft(y).ticks(5));
+    void select(gy.current).call(axisLeft(y).ticks(5, "$.0f"));
   }, [gy, y]);
 
   return (
-    <svg
-      width={width}
-      height={height}
-      viewBox={`0 0 ${width} ${height}`}
-      style={{ maxWidth: "100%", height: "auto" }}>
-      <g ref={gx} transform={`translate(0,${height - marginBottom})`}></g>
-      <g ref={gy} transform={`translate(${marginLeft},0)`}></g>
-      <path fill="steelblue" stroke="currentColor" strokeWidth="1.5" d={chartArea(areaData) as string} />
-    </svg>
+    <>
+      <svg
+        width={width}
+        height={height}
+        viewBox={`0 0 ${width} ${height}`}
+        style={{ maxWidth: "100%", height: "auto" }}>
+        <g ref={gx} transform={`translate(0,${height - marginBottom})`}></g>
+        <g ref={gy} transform={`translate(${marginLeft},0)`}></g>
+        <path fill="steelblue" stroke="currentColor" strokeWidth="1.5" d={chartArea(areaData) as string} />
+      </svg>
+    </>
   );
 }
 
