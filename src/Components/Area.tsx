f8207e34c01e1f8bb0b583de88c1a26c2e5900dc
PabloPicas0 <pawel.fe@gmail.com>
2024-06-13 13:46:09 +0200

    fix: inconsistent chart scope dates

--- src/Components/Area.tsx
+++ src/Components/Area.tsx
@@ -1,9 +1,18 @@
-import { area, select, scaleLinear, extent, max, axisBottom, axisLeft, scaleUtc, curveBasis } from "d3";
+import {
+  area,
+  select,
+  scaleLinear,
+  extent,
+  max,
+  axisBottom,
+  axisLeft,
+  scaleUtc,
+  curveBasis,
+} from "d3";
 import { useEffect, useMemo, useRef, useState } from "react";
 
 import { AreaPropsModel } from "../models/AreaPropsModel";
 
-// FIX: There is something wrong with charts date 
 function Area({
   data,
   width = 800,
@@ -13,16 +22,19 @@ function Area({
   marginBottom = 20,
   marginLeft = 40,
 }: AreaPropsModel) {
-  const ONE_WEEK = data.length - 24 * 7;
-  const ONE_MONTH = data.length - 24 * 31;
-  const ALL_TIME = 0;
+  const TODAY = useMemo(() => Date.now(), []);
+  const ONE_MONTH = 31;
+  const ONE_WEEK = 7;
+  const ALL_TIME = useMemo(() => TODAY, []);
 
-  const [someTimeAgo, setSomeTimeAgo] = useState(ALL_TIME);
+  const [scope, setScope] = useState(ALL_TIME);
 
   // This copies data from 7 days, 31 days or overall
-  const chart = data.slice(someTimeAgo);
+  const timestamp = TODAY - scope * 24 * 60 * 60 * 1000;
+  const scopeIndex = data.findIndex((date) => new Date(date[0]).getTime() >= timestamp);
+  const chart = data.slice(scopeIndex);
 
-  const timeScale = useMemo(() => chart.map((time) => new Date(time[0])), [someTimeAgo]);
+  const timeScale = chart.map((time) => new Date(time[0]))
   const areaData = chart.map((vector) => ({ date: new Date(vector[0]), price: vector[1] }));
 
   const gx = useRef<SVGGElement>(null);
@@ -35,7 +47,7 @@ function Area({
     (d) => x(d.date),
     y(0),
     (d) => y(d.price)
-  ).curve(curveBasis);
+  )
 
   useEffect(() => {
     if (!gx.current) return;
@@ -67,11 +79,12 @@ function Area({
         <path fill="steelblue" stroke="currentColor" strokeWidth="1.5" d={chartArea(areaData) as string} />
       </svg>
 
-      <div style={{ display: "flex", justifyContent: "end", gap: "20px", zIndex: "20", alignItems: "center" }}>
-        <span>Scope chart:</span>
-        <button onClick={() => setSomeTimeAgo(ONE_WEEK)}>Weekly</button>
-        <button onClick={() => setSomeTimeAgo(ONE_MONTH)}>Monthly</button>
-        <button onClick={() => setSomeTimeAgo(ALL_TIME)}>Overall</button>
+      <div
+        style={{ display: "flex", justifyContent: "end", gap: "20px", zIndex: "20", alignItems: "center" }}>
+        <span>Chart scope:</span>
+        <button onClick={() => setScope(ONE_WEEK)}>Weekly</button>
+        <button onClick={() => setScope(ONE_MONTH)}>Monthly</button>
+        <button onClick={() => setScope(ALL_TIME)}>Overall</button>
       </div>
     </>
   );
