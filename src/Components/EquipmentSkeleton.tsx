6bc87508fa9fa4717462508d5144654e2864ee3e
PabloPicas0 <pawel.fe@gmail.com>
2024-07-04 13:58:00 +0200

    refactor: adjust looks of Equipment skeleton to look more like actual one

--- src/Components/EquipmentSkeleton.tsx
+++ src/Components/EquipmentSkeleton.tsx
@@ -1,11 +1,19 @@
 function EquipmentSkeleton() {
   return (
     <section className="items-container">
-      <h2 className="skeleton-text" style={{ gridColumn: "1 / -1", placeSelf: "center" }}></h2>
-      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
-      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
-      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
-      <div className="skeleton-rect" style={{ placeSelf: "center" }}></div>
+      <h2
+        className="skeleton-text"
+        style={{ gridColumn: "1 / -1", placeSelf: "center", height: "30px", borderRadius: "1.5rem" }}></h2>
+      <h2
+        className="skeleton-text"
+        style={{ gridColumn: "1 / -1", placeSelf: "center", height: "30px", borderRadius: "1.5rem", marginBottom: "0.1rem" }}></h2>
+      <h2
+        className="skeleton-text"
+        style={{ gridColumn: "1 / -1", placeSelf: "center", height: "36px", width: "230px" }}></h2>
+
+        {[...new Array(4)].map((_, idx) => {
+          return <div className="skeleton-rect" style={{ placeSelf: "center", height: "273px", width: "429px" }}></div>
+        })}
     </section>
   );
 }
