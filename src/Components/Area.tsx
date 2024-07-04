8beac57b85c8a27310fa1b22753394fea3be529a
PabloPicas0 <pawel.fe@gmail.com>
2024-07-04 15:56:09 +0200

    feat: add line for taget price selection

--- src/Components/Area.tsx
+++ src/Components/Area.tsx
@@ -1,14 +1,4 @@
-import {
-  area,
-  select,
-  scaleLinear,
-  extent,
-  max,
-  axisBottom,
-  axisLeft,
-  scaleUtc,
-  curveBasis,
-} from "d3";
+import { area, select, scaleLinear, extent, max, axisBottom, axisLeft, scaleUtc, pointer } from "d3";
 import { useEffect, useMemo, useRef, useState } from "react";
 
 import { AreaPropsModel } from "../models/AreaPropsModel";
@@ -20,7 +10,7 @@ function Area({
   marginTop = 20,
   marginRight = 10,
   marginBottom = 20,
-  marginLeft = 40,
+  marginLeft = 45,
 }: AreaPropsModel) {
   const TODAY = useMemo(() => Date.now(), []);
   const ONE_MONTH = 31;
@@ -28,13 +18,14 @@ function Area({
   const ALL_TIME = useMemo(() => TODAY, []);
 
   const [scope, setScope] = useState(ALL_TIME);
+  const [rectY, setRectY] = useState(50);
 
   // This copies data from 7 days, 31 days or overall
   const timestamp = TODAY - scope * 24 * 60 * 60 * 1000;
   const scopeIndex = data.findIndex((date) => new Date(date[0]).getTime() >= timestamp);
   const chart = data.slice(scopeIndex);
 
-  const timeScale = chart.map((time) => new Date(time[0]))
+  const timeScale = chart.map((time) => new Date(time[0]));
   const areaData = chart.map((vector) => ({ date: new Date(vector[0]), price: vector[1] }));
 
   const gx = useRef<SVGGElement>(null);
@@ -47,7 +38,7 @@ function Area({
     (d) => x(d.date),
     y(0),
     (d) => y(d.price)
-  )
+  );
 
   useEffect(() => {
     if (!gx.current) return;
@@ -75,7 +66,28 @@ function Area({
         viewBox={`0 0 ${width} ${height}`}
         style={{ maxWidth: "100%", height: "auto" }}>
         <g ref={gx} transform={`translate(0,${height - marginBottom})`}></g>
-        <g ref={gy} transform={`translate(${marginLeft},0)`}></g>
+        <g ref={gy} transform={`translate(${marginLeft},0)`}>
+          <rect
+            fill="red"
+            x={0}
+            y={rectY}
+            width={width}
+            height={10}
+            style={{ cursor: "pointer" }}
+            onDragCapture={(e) => {
+              const [ex, ey] = pointer(e);
+              const mouseData = y.invert(ey);
+
+              if (y(mouseData) < marginTop || y(mouseData) > height - (marginBottom + 10)) {
+                return;
+              }
+
+              setRectY(ey);
+
+              console.log(y(mouseData));
+            }}
+          />
+        </g>
         <path fill="steelblue" stroke="currentColor" strokeWidth="1.5" d={chartArea(areaData) as string} />
       </svg>
 
