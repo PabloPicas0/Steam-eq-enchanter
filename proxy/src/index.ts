da6bb08d2e2d88477868023a1bd4e9bd403e3572
PabloPicas0 <pawel.fe@gmail.com>
2024-08-12 21:59:38 +0200

    feat: add steam login session

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -3,7 +3,9 @@ import cors from "cors";
 
 import { AddressInfo } from "net";
 
-import SteamUser from "steam-user"
+import SteamUser from "steam-user";
+import { LoginSession, EAuthTokenPlatformType } from "steam-session";
+
 import getUserInfo from "./utils/getUserInfo.js";
 import getUserInventory from "./utils/getUserInventory.js";
 import getPriceOverview from "./utils/getPriceOverview.js";
@@ -17,7 +19,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const session = new LoginSession(EAuthTokenPlatformType.WebBrowser);
+try {
+  await session.startWithCredentials({
+    accountName: process.env.STEAM_ACCOUNT_NAME,
+    password: process.env.STEAM_PASS,
+  });
+  await session.submitSteamGuardCode(process.env.CODE);
+
+  console.log("successfully logged to steam with ID", session.steamID);
+} catch (error) {
+  console.error(error);
+}
+
 const user = new SteamUser();
+
+// user.logOn({ refreshToken: session.refreshToken });
 
 app.get("/", async (req, res) => {
   const { id } = req.query;
@@ -29,7 +46,7 @@ app.get("/", async (req, res) => {
     ]);
 
     // label for each inventory item name and icon
-    addItemDescriptions(userInventory)
+    addItemDescriptions(userInventory);
 
     const filteredUserInventory = userInventory.assets.filter(
       (item) => !item.type.includes("Collectible") && !item.type.includes("Music")
@@ -65,7 +82,7 @@ app.post("/", async (req, res) => {
       if (priceHistory[idx].success) {
         priceHistory[idx].prices.forEach((price) => (price[1] = Number(price[1] / POLISH_CURRENCY_PRICE))); // convert price history from PLN to USD
       }
-      
+
       priceHistory[idx].price_suffix = "USD";
       itemPrice.price_history = priceHistory[idx];
 
