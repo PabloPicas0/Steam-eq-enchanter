223693c3886b38325e7d4c5b5eee0fcb43cca4c6
PabloPicas0 <pawel.fe@gmail.com>
2024-08-15 19:34:06 +0200

    refactor: move url to separate var

--- proxy/src/utils/getUserInventory.ts
+++ proxy/src/utils/getUserInventory.ts
@@ -1,12 +1,13 @@
 import { UserInvenotryType } from "../models/UserInventoryModel.js";
 
 async function getUserInventory(id: string) {
-  const inventory = await fetch(`http://steamcommunity.com/inventory/${id}/730/2?l=english&count=5000`);
+  const url = `http://steamcommunity.com/inventory/${id}/730/2?l=english&count=5000`;
+  
+  const inventory = await fetch(url);
 
   if (!inventory.ok) throw new Error("Unable to fetch equipment");
 
   const inventoryData: UserInvenotryType = await inventory.json();
-
 
   // Remove duplicates
   const ids = inventoryData.assets.map((item) => item.classid);
