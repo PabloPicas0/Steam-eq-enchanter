c0422d984149e5dd78d189deacca5091af7dd89a
PabloPicas0 <pawel.fe@gmail.com>
2024-09-05 15:41:40 +0200

    feat: add filter types and logic for disable/eneble them

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+
 import FilterIcon from "../Icons/FilterIcon";
 
-function Filter(props: { className: string }) {
-  const { className } = props;
+import "../styles/Filter.css";
+
+const itemQualityTypes = [
+  ["Common", "rgb(176, 195, 217)"],
+  ["Uncommon", "rgb(94, 152, 217)"],
+  ["Rare", "rgb(75, 105, 255)"],
+  ["Mythical", "rgb(136, 71, 255)"],
+  ["Legendary", "rgb(211, 44, 230)"],
+  ["Ancient", "rgb(235, 75, 75)"],
+  ["Special", "rgb(134 80 172)"],
+  ["Contraband", "rgb(228, 174, 57)"],
+];
+
+function Filter() {
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <button className={className}>
-      Filter <FilterIcon width={15} height={15} />
-    </button>
+    <div className="filter">
+      <button className="filter-btn" onClick={() => setIsClicked((prev) => !prev)}>
+        Filter <FilterIcon width={15} height={15} />
+      </button>
+
+      <div
+        className="filter-options-wrapper"
+        style={{ visibility: isClicked ? "visible" : "hidden", opacity: isClicked ? 1 : 0 }}>
+        {itemQualityTypes.map((type) => {
+          const [name, color] = type;
+
+          return (
+            <div className="filter-item" style={{ color: color }}>
+              <input type="checkbox" id={name} />
+              <label htmlFor={name}>{name}</label>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
