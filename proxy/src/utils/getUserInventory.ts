c82ddd2f3aaf2802ec6b1c84d225f14ed0b5b4e7
PabloPicas0 <pawel.fe@gmail.com>
2024-09-06 17:53:46 +0200

    feat: add better duplicate removal and info about item ammount

--- proxy/src/utils/getUserInventory.ts
+++ proxy/src/utils/getUserInventory.ts
@@ -1,17 +1,31 @@
-import { UserInvenotryType } from "../models/UserInventoryModel.js";
+import { UserInventoryType } from "../models/UserInventoryModel.js";
 
 async function getUserInventory(id: string) {
   const url = `http://steamcommunity.com/inventory/${id}/730/2?l=english&count=5000`;
-  
+
   const inventory = await fetch(url);
 
   if (!inventory.ok) throw new Error("Unable to fetch equipment");
 
-  const inventoryData: UserInvenotryType = await inventory.json();
+  const inventoryData: UserInventoryType = await inventory.json();
 
-  // Remove duplicates
   const ids = inventoryData.assets.map((item) => item.classid);
-  const filteredAssets = inventoryData.assets.filter((item, idx) => !ids.includes(item.classid, idx + 1));
+  const itemsAmmount: { [key: string]: number } = ids.reduce((acc, id) => {
+    acc[id] = acc[id] + 1 || 1;
+
+    return acc;
+  }, {});
+
+  // Remove duplicates and add item ammount data
+  const filteredIds = Array.from(new Set(ids));
+  const filteredAssets = filteredIds.map((id) => {
+    const item = inventoryData.assets.find((item) => item.classid === id);
+    const classid = item.classid;
+
+    item.amount = itemsAmmount[classid].toString();
+
+    return item;
+  });
 
   inventoryData.assets = filteredAssets;
   inventoryData.total_inventory_count = filteredAssets.length;
