364d96513d690956439045c625e6cbaf1371d58d
PabloPicas0 <pawel.fe@gmail.com>
2024-09-07 18:31:08 +0200

    feat: add types for many curriencies table

--- proxy/src/models/CurrencyModel.ts
+++ proxy/src/models/CurrencyModel.ts
@@ -4,3 +4,10 @@ export type CurrencyModel = {
   code: string;
   rates: { no: string; effectiveDate: string; mid: number }[];
 };
+
+export type CurrencyTableModel = {
+  table: string;
+  no: string;
+  effectiveDate: string;
+  rates: { currency: string; code: string; mid: number }[];
+}[];
