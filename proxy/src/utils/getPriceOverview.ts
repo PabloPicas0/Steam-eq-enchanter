0b40543dd6441e0c80c0dbd84de5332d796ea667
PabloPicas0 <pawel.fe@gmail.com>
2024-09-10 19:31:11 +0200

    feat: add better type annotation, addtional string query and separate url to different var

--- proxy/src/utils/getPriceOverview.ts
+++ proxy/src/utils/getPriceOverview.ts
@@ -3,14 +3,13 @@ import { MarketModel } from "../models/MarketModel.js";
 async function getPriceOverview(market_hash_name: string) {
   // Some items have "&" which is not accepted by Steam API
   // Instead it need to be replaced with "%26"
-  const priceOverviewResponse = await fetch(
-    `https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name=${market_hash_name.replace(
-      "&",
-      "%26"
-    )}`
-  );
+  const url = `https://steamcommunity.com/market/priceoverview/?currency=6&appid=730&market_hash_name=${market_hash_name.replace(
+    "&",
+    "%26"
+  )}`;
+  const priceOverviewResponse = await fetch(url);
 
-  const priceOverview: MarketModel = await priceOverviewResponse.json();
+  const priceOverview = (await priceOverviewResponse.json()) as MarketModel;
 
   return priceOverview;
 }
