a71dee0cb34e58c320e30e92b5c9dfcd679d3d31
PabloPicas0 <pawel.fe@gmail.com>
2024-09-28 16:49:04 +0200

    feat: create Favourite component

--- src/Components/Favourite.tsx
+++ src/Components/Favourite.tsx
@@ -1,9 +1,16 @@
-import { ReactNode } from "react";
+import { useState } from "react";
+import HeartIcon from "../Icons/Heartcon";
 
-function Favourite(props: { children: ReactNode; className?: string }) {
-  const { children, className } = props;
+function Favourite(props: { className?: string }) {
+  const { className } = props;
 
-  return <button className={className}>{children}</button>;
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  return (
+    <button className={className} onClick={() => setIsFavourite((prev) => !prev)}>
+      <HeartIcon width="20px" height="20px" color="#ff0000" fill={isFavourite ? "red" : ""} />
+    </button>
+  );
 }
 
 export default Favourite;
