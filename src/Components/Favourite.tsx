40c7376afc6c51c923b50013a707fabf766026ba
PabloPicas0 <pawel.fe@gmail.com>
2024-09-29 18:37:53 +0200

    feat: add logic for adding, saving and removing favourite item

--- src/Components/Favourite.tsx
+++ src/Components/Favourite.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
 import HeartIcon from "../Icons/Heartcon";
 
-function Favourite(props: { className?: string }) {
-  const { className } = props;
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { addToFavouriteItems, removeFromFavouriteItems } from "../Store/Slices/profileSlice";
+import { useAppSelector } from "../hooks/useAppSelector ";
 
-  const [isFavourite, setIsFavourite] = useState(false);
+function Favourite(props: { className?: string; itemID: string }) {
+  const { className, itemID } = props;
+
+  const favourite = useAppSelector((state) => state.profile.favouriteItems);
+  const isFavourite = favourite.includes(itemID);
+  const handleFavourite = isFavourite ? removeFromFavouriteItems : addToFavouriteItems;
+
+  const dispatch = useAppDispatch();
 
   return (
-    <button className={className} onClick={() => setIsFavourite((prev) => !prev)}>
+    <button className={className} onClick={() => dispatch(handleFavourite(itemID))}>
       <HeartIcon width="20px" height="20px" color="#ff0000" fill={isFavourite ? "red" : ""} />
     </button>
   );
