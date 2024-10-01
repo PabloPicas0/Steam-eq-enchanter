a7870a18d9e53749fbfbe4d5f7048e7519405c5e
PabloPicas0 <pawel.fe@gmail.com>
2024-10-01 17:23:58 +0200

    refactor: change prop name

--- src/Components/Favourite.tsx
+++ src/Components/Favourite.tsx
@@ -4,17 +4,17 @@ import { useAppDispatch } from "../hooks/useAppDispatch";
 import { addToFavouriteItems, removeFromFavouriteItems } from "../Store/Slices/profileSlice";
 import { useAppSelector } from "../hooks/useAppSelector ";
 
-function Favourite(props: { className?: string; itemID: string }) {
-  const { className, itemID } = props;
+function Favourite(props: { className?: string; itemName: string }) {
+  const { className, itemName } = props;
 
-  const isFavourite = useAppSelector((state) => state.profile.favouriteItems.includes(itemID));
-  
+  const isFavourite = useAppSelector((state) => state.profile.favouriteItems.includes(itemName));
+
   const handleFavourite = isFavourite ? removeFromFavouriteItems : addToFavouriteItems;
 
   const dispatch = useAppDispatch();
 
   return (
-    <button className={className} onClick={() => dispatch(handleFavourite(itemID))}>
+    <button className={className} onClick={() => dispatch(handleFavourite(itemName))}>
       <HeartIcon width="20px" height="20px" color="#ff0000" fill={isFavourite ? "red" : ""} />
     </button>
   );
