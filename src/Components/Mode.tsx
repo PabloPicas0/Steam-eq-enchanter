fba23dce1873b1a8d58b5802c1e17888044f327f
PabloPicas0 <pawel.fe@gmail.com>
2024-10-10 14:51:03 +0200

    refactor: name changes

--- src/Components/Mode.tsx
+++ src/Components/Mode.tsx
@@ -1,11 +1,11 @@
 import { useAppSelector } from "../hooks/useAppSelector ";
-import MarketItems from "./MarketItems";
+import MarketItemsSearch from "./MarketItemsSearch";
 import Equipment from "./Equipment";
 
 function Mode() {
-  const isUserEquipment = useAppSelector((state) => state.profile.isUserEquipment);
+  const isUserEquipment = useAppSelector((state) => state.itemsFromMarket.isUserEquipment);
 
-  return isUserEquipment ? <Equipment /> : <MarketItems />;
+  return isUserEquipment ? <Equipment /> : <MarketItemsSearch />;
 }
 
 export default Mode
