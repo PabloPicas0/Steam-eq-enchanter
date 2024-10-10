937bb9182d0a75ee45320d371990ac527639e21d
PabloPicas0 <pawel.fe@gmail.com>
2024-10-10 14:51:48 +0200

    feat: add itemsFromMarket reducer to store

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "./Slices/profileSlice";
+import itemsFromMarketReducer from "./Slices/itemsFromMarketSlice";
 
 export const store = configureStore({
   reducer: {
     profile: profileReducer,
+    itemsFromMarket: itemsFromMarketReducer,
   },
 });
 
 // Get the type of our store variable
-export type AppStore = typeof store
+export type AppStore = typeof store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
+export type RootState = ReturnType<AppStore["getState"]>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = AppStore['dispatch']
+export type AppDispatch = AppStore["dispatch"];
