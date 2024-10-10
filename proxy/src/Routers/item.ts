0954a3242d5bafb137b78f689db8cf3db1f15186
PabloPicas0 <pawel.fe@gmail.com>
2024-10-10 13:50:27 +0200

    feat: create separate routers

---
+++ proxy/src/Routers/item.ts
@@ -0,0 +1,19 @@
+import express, { Request } from "express";
+import { AdditionalItemModel } from "../models/AdditionalItemModel.js";
+
+export const router = express.Router({ mergeParams: true });
+
+router.get("/", async (req: Request<{ name: string }>, res) => {
+  const name = req.params.name.replace(/[&]/g, "%26").replace(/\s/g, "%20");
+  const url = `https://steamcommunity.com/market/search/render?norender=1&appid=730&query=${name}`;
+
+  try {
+    const itemReq = await fetch(url);
+    const item = await itemReq.json() as AdditionalItemModel;
+
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
---
+++ proxy/src/Routers/root.ts
@@ -0,0 +1,75 @@
+import express from "express";
+
+import addItemDescriptions from "../utils/addItemDescriptions.js";
+import getUserInfo from "../utils/getUserInfo.js";
+import getUserInventory from "../utils/getUserInventory.js";
+import getPriceHistory from "../utils/getPriceHistory.js";
+import getPriceOverview from "../utils/getPriceOverview.js";
+
+import { cookieValue, webCookies } from "../index.js";
+
+export const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    const [userInfo, userInventory] = await Promise.all([
+      getUserInfo(process.env.STEAM_KEY, `${id}`),
+      getUserInventory(`${id}`),
+    ]);
+
+    // label for each inventory item name and icon
+    addItemDescriptions(userInventory);
+
+    const filteredUserInventory = userInventory.assets.filter(
+      (item) => !item.type.includes("Collectible") && !item.type.includes("Music")
+    );
+
+    userInventory.assets = filteredUserInventory;
+    userInventory.total_inventory_count = filteredUserInventory.length;
+
+    res.send([userInfo, userInventory]);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const marketItems: string[] = req.body;
+
+  if (!webCookies) res.sendStatus(404);
+
+  try {
+    const priceOverview = await Promise.all(
+      marketItems.map((item) => {
+        return getPriceOverview(item);
+      })
+    );
+
+    const priceHistory = await Promise.all(
+      marketItems.map((item) => {
+        return getPriceHistory(item, { headers: { Cookie: `steamLoginSecure=${cookieValue}` } });
+      })
+    );
+
+    const finalPrice = priceOverview.map((itemPrice, idx) => {
+      const { lowest_price, median_price, success } = itemPrice;
+
+      itemPrice.price_history = priceHistory[idx];
+
+      if (success) {
+        itemPrice.lowest_price = lowest_price.replace("zł", "").replace(",", ".");
+        itemPrice.median_price = median_price.replace("zł", "").replace(",", ".");
+      }
+
+      return itemPrice;
+    });
+
+    res.send(finalPrice);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
