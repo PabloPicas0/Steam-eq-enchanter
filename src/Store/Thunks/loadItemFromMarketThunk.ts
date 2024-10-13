f33aba8b5e26d447ad9982b410a9a747050c6a36
PabloPicas0 <pawel.fe@gmail.com>
2024-10-13 15:15:14 +0200

    feat: add better thunk validation

--- src/Store/Thunks/loadItemFromMarketThunk.ts
+++ src/Store/Thunks/loadItemFromMarketThunk.ts
@@ -4,15 +4,31 @@ import { RootState } from "../store";
 
 export const loadItemFromMarket = createAsyncThunk<AdditionalItemModel, string, { state: RootState }>(
   "itemsFromMarket/loadItemFromMarket",
-  async (itemName: string) => {
-    const req = await fetch(`/api/item/${itemName}`);
+  async (link: string, { getState, rejectWithValue }) => {
+    try {
+      const { marketItems } = getState().itemsFromMarket;
 
-    if (!req.ok) {
-      throw new Error(req.statusText);
+      const linksParts = link.split("/");
+      const lastPart = linksParts.length - 1;
+      const itemName = decodeURIComponent(linksParts[lastPart]);
+
+      if (
+        marketItems.length === 10 ||
+        marketItems.some((item) => item.results[0].hash_name === itemName)
+      )
+        return rejectWithValue(false);
+
+      const req = await fetch(`/api/market/item/${itemName}`);
+
+      if (!req.ok) {
+        throw new Error(req.statusText);
+      }
+
+      const item = (await req.json()) as AdditionalItemModel;
+
+      return item;
+    } catch (error) {
+      return rejectWithValue(error);
     }
-
-    const item = (await req.json()) as AdditionalItemModel;
-
-    return item;
   }
 );
