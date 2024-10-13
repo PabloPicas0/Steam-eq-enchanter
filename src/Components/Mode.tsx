605d3594a259e9d1767bb6dff4ea377c5406d9b4
PabloPicas0 <pawel.fe@gmail.com>
2024-10-13 15:13:45 +0200

    refactor: add renamed market component

--- src/Components/Mode.tsx
+++ src/Components/Mode.tsx
@@ -1,11 +1,11 @@
 import { useAppSelector } from "../hooks/useAppSelector ";
-import MarketItemsSearch from "./MarketItemsSearch";
+import Market from "./Market";
 import Equipment from "./Equipment";
 
 function Mode() {
   const isUserEquipment = useAppSelector((state) => state.itemsFromMarket.isUserEquipment);
 
-  return isUserEquipment ? <Equipment /> : <MarketItemsSearch />;
+  return isUserEquipment ? <Equipment /> : <Market />;
 }
 
 export default Mode
