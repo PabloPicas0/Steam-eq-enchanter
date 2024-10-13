70b11a50b53479c88fc94a21ed3b5e0c6af12849
PabloPicas0 <pawel.fe@gmail.com>
2024-10-13 15:11:54 +0200

    refactor: split component into separate sub-components

---
+++ src/Components/Market.tsx
@@ -0,0 +1,17 @@
+import "../styles/MarketItemsSearch.css";
+import MarketItems from "./MarketItems";
+
+import MarketSearchBar from "./MarketSearchBar";
+
+// TODO: Lack of fetching error validation for user
+// TODO: Currency needs to match with this in eq
+function Market() {
+  return (
+    <section>
+      <MarketSearchBar />
+      <MarketItems />
+    </section>
+  );
+}
+
+export default Market;
