83ca3d62dc85a96fb999f5cbc5b3068a737d5c11
PabloPicas0 <pawel.fe@gmail.com>
2024-10-15 14:27:52 +0200

    feat: add spinner for initiating item loading and remove item functionality

--- src/Components/Market/MarketItems.tsx
+++ src/Components/Market/MarketItems.tsx
@@ -1,19 +1,27 @@
+import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { useAppSelector } from "../../hooks/useAppSelector ";
+import { removeMarketitem } from "../../Store/Slices/itemsFromMarketSlice";
 
 function MarketItems() {
   const items = useAppSelector((state) => state.itemsFromMarket.marketItems);
   const pending = useAppSelector((state) => state.itemsFromMarket.pending);
 
+  const dispatch = useAppDispatch();
+
   return (
     <>
-      <h2>Items {items.length}/10</h2>
+      <h2 style={{ color: "white" }}>Items {items.length}/10</h2>
+
       <div className="items-from-market-wrapper">
         {items.map((item, idx) => {
           const { sell_price_text } = item.results[0];
           const { icon_url, classid, name } = item.results[0].asset_description;
 
           return (
-            <div className="item-from-market" key={classid + idx}>
+            <div
+              className="item-from-market"
+              key={classid + idx}
+              onClick={() => dispatch(removeMarketitem(classid))}>
               <img
                 src={`https://steamcommunity-a.akamaihd.net/economy/image//${icon_url}`}
                 width={100}
@@ -21,14 +29,14 @@ function MarketItems() {
               />
 
               <div>
-                <p>{name + " " + idx}</p>
+                <p>{name}</p>
                 <p>{sell_price_text}</p>
               </div>
             </div>
           );
         })}
 
-        {pending && <div className="skeleton-rect" style={{ width: "auto", height: "auto" }} />}
+        {pending && <span className="loader"></span>}
       </div>
     </>
   );
