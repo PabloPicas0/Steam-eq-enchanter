bf711cb3ded4435f3661ccd67179aa642f874488
PabloPicas0 <pawel.fe@gmail.com>
2024-10-16 20:28:24 +0200

    feat: on each market item add render Item component

--- src/Components/Market/MarketItems.tsx
+++ src/Components/Market/MarketItems.tsx
@@ -1,12 +1,9 @@
-import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { useAppSelector } from "../../hooks/useAppSelector ";
-import { removeMarketitem } from "../../Store/Slices/itemsFromMarketSlice";
+import Item from "./Item";
 
 function MarketItems() {
   const items = useAppSelector((state) => state.itemsFromMarket.marketItems);
   const pending = useAppSelector((state) => state.itemsFromMarket.pending);
-
-  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -14,26 +11,9 @@ function MarketItems() {
 
       <div className="items-from-market-wrapper">
         {items.map((item, idx) => {
-          const { sell_price_text } = item.results[0];
-          const { icon_url, classid, name } = item.results[0].asset_description;
+          const { classid } = item.results[0].asset_description;
 
-          return (
-            <div
-              className="item-from-market"
-              key={classid + idx}
-              onClick={() => dispatch(removeMarketitem(classid))}>
-              <img
-                src={`https://steamcommunity-a.akamaihd.net/economy/image//${icon_url}`}
-                width={100}
-                height={100}
-              />
-
-              <div>
-                <p>{name}</p>
-                <p>{sell_price_text}</p>
-              </div>
-            </div>
-          );
+          return <Item item={item} key={classid + idx} />;
         })}
 
         {pending && <span className="loader"></span>}
