df7c60cfa5e61353c80d894c0abfe275dd494f35
PabloPicas0 <pawel.fe@gmail.com>
2024-10-17 18:28:35 +0200

    refactor: price history is now in same currency as current item price

--- src/Components/Market/MarketItems.tsx
+++ src/Components/Market/MarketItems.tsx
@@ -1,19 +1,30 @@
+import { useMemo, useState } from "react";
 import { useAppSelector } from "../../hooks/useAppSelector ";
-import Item from "./Item";
+import MarketItem from "./MarketItem";
+import getCorrectMarketCurrency from "../../utils/getCorrectMarketCurrency";
 
 function MarketItems() {
   const items = useAppSelector((state) => state.itemsFromMarket.marketItems);
+  const currencies = useAppSelector((state) => state.profile.currencies);
   const pending = useAppSelector((state) => state.itemsFromMarket.pending);
+
+  const [currencyCode, setCurrencyCode] = useState("USD");
+
+  const pickedCurrencyData = currencies[0].rates.find((rate) => rate.code === currencyCode);
+  const itemsWithCorrecetedCurrency = useMemo(
+    () => getCorrectMarketCurrency(items, pickedCurrencyData),
+    [items, currencyCode]
+  );
 
   return (
     <>
       <h2 style={{ color: "white" }}>Items {items.length}/10</h2>
 
       <div className="items-from-market-wrapper">
-        {items.map((item, idx) => {
+        {itemsWithCorrecetedCurrency.map((item, idx) => {
           const { classid } = item.results[0].asset_description;
 
-          return <Item item={item} key={classid + idx} />;
+          return <MarketItem item={item} key={classid + idx} />;
         })}
 
         {pending && <span className="loader"></span>}
