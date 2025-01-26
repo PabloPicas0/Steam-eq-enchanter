f1353cb9f9be9a045dc07e5cf0f97a6bbd7d43de
PabloPicas0 <pawel.fe@gmail.com>
2025-01-26 16:13:20 +0100

    feat: add scroll to top functionality

--- src/Components/ScrollToTop.tsx
+++ src/Components/ScrollToTop.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import GoToTopIcon from "../Icons/GoToTop";
-import "../styles/ScrollToTop.css"
+import "../styles/ScrollToTop.css";
 
 function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const visibilityState = isVisible ? "visible" : "hidden";
+  const opacityState = isVisible ? 1 : 0;
+  const scrollToTop = () => document.body.scrollTo({ top: 0, behavior: "smooth" });
+
+  useEffect(() => {
+    const body = document.body;
+    const isScrolled = () => (body.scrollTop > 300 ? setIsVisible(true) : setIsVisible(false));
+
+    body.addEventListener("scroll", isScrolled);
+
+    return () => body.removeEventListener("scroll", isScrolled);
+  }, []);
+
   return (
-    <div className="scroll-wrapper">
+    <div
+      className="scroll-wrapper scroll-transition"
+      style={{ visibility: visibilityState, opacity: opacityState }}
+      onClick={scrollToTop}>
       <GoToTopIcon width="3rem" height="3rem" />
     </div>
   );
