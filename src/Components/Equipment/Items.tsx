8b608a6ead4f2836ef41bfba6a4d2b92f56cd6e1
PabloPicas0 <pawel.fe@gmail.com>
2025-01-26 15:15:36 +0100

    refactor: delete pagination

--- src/Components/Equipment/Items.tsx
+++ src/Components/Equipment/Items.tsx
@@ -1,4 +1,3 @@
-import usePagination from "../../hooks/usePagination";
 import { EquipmentModel } from "../../models/EquipmentModel";
 import UserItem from "./UserItem";
 
@@ -9,9 +8,6 @@ type PropTypes = {
 function Items(props: PropTypes) {
   const { filteredItems } = props;
 
-  const { pagination, setPagination, moveBackwards, moveForeword } = usePagination(filteredItems.length);
-  const { start, end } = pagination;
-
   return (
     <>
       <ul className="items-container">
@@ -19,14 +15,6 @@ function Items(props: PropTypes) {
           <UserItem key={item.market_name} item={item} isSelected={false} />
         ))}
       </ul>
-
-      <div className="pagination">
-        <button onClick={() => setPagination(moveBackwards)}>Back</button>
-
-        {`${start} / ${end} of ${filteredItems.length}`}
-
-        <button onClick={() => setPagination(moveForeword)}>Next</button>
-      </div>
     </>
   );
 }
