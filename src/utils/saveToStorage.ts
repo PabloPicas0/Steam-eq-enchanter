c5f0138b2a058f5bac6761aa6bc0392c364455d2
PabloPicas0 <pawel.fe@gmail.com>
2025-01-27 15:11:49 +0100

    refactor: add time when price was changed

--- src/utils/saveToStorage.ts
+++ src/utils/saveToStorage.ts
@@ -1,9 +1,24 @@
-function saveToStorage(classid: string, targetPrice: number) {
-  const prices = JSON.parse(localStorage.getItem("prices") || "{}") as { [key: string]: number };
+type Prices = {
+  [key: string]:
+    | {
+        targetPrice: number;
+        created: number;
+        lastModified: number;
+      }
+    | undefined;
+};
 
-  prices[classid] = targetPrice;
+function saveToStorage(classid: string, targetPrice: number) {
+  const current = new Date().getTime();
+  const prices: Prices = JSON.parse(localStorage.getItem("prices") || "{}");
+
+  if (!prices[classid])
+    prices[classid] = { targetPrice: targetPrice, created: current, lastModified: current };
+
+  prices[classid].targetPrice = targetPrice;
+  prices[classid].lastModified = current;
 
   localStorage.setItem("prices", JSON.stringify(prices));
 }
 
-export default saveToStorage
+export default saveToStorage;
