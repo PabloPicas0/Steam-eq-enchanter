ec6c7634dd35091b5d5ff78e46f41e8fa9d24a37
PabloPicas0 <pawel.fe@gmail.com>
2025-01-27 21:21:20 +0100

    refactor: auto clear oldest price from storage after 50 items

--- src/utils/getSavedItemProps.ts
+++ src/utils/getSavedItemProps.ts
@@ -1,14 +1,16 @@
+import { StoragePrices } from "../models/StoragePricesModel";
+
 function getSavedItemProps(classid: string) {
   const storage = localStorage.getItem("prices");
 
   if (!storage) return 0.03;
 
-  const prices = JSON.parse(storage) as { [key: string]: number | undefined };
+  const prices: StoragePrices = JSON.parse(storage);
   const price = prices[classid];
 
   if (!price) return 0.03;
 
-  return price;
+  return price.targetPrice;
 }
 
 export default getSavedItemProps;
--- src/utils/saveToStorage.ts
+++ src/utils/saveToStorage.ts
@@ -1,19 +1,33 @@
-type Prices = {
-  [key: string]:
-    | {
-        targetPrice: number;
-        created: number;
-        lastModified: number;
-      }
-    | undefined;
-};
+import { StoragePrices } from "../models/StoragePricesModel";
 
 function saveToStorage(classid: string, targetPrice: number) {
   const current = new Date().getTime();
-  const prices: Prices = JSON.parse(localStorage.getItem("prices") || "{}");
+  const prices: StoragePrices = JSON.parse(localStorage.getItem("prices") || "{}");
 
-  if (!prices[classid])
+  if (!prices[classid]) {
+    const pricesKeys = Object.keys(prices)
+
+    // If there is more than 50 prices
+    // Delete oldest one
+    if (pricesKeys.length >= 50) {
+      let oldestLastModification = Number.POSITIVE_INFINITY
+      let oldestKey = "";
+
+      for (const key of pricesKeys) {
+        const currentLastModification = prices[key].lastModified
+
+        if (oldestLastModification > currentLastModification) {
+          oldestLastModification = currentLastModification;
+          oldestKey = key;
+        }
+      }
+
+      delete prices[oldestKey]
+    }
+
     prices[classid] = { targetPrice: targetPrice, created: current, lastModified: current };
+  }
+    
 
   prices[classid].targetPrice = targetPrice;
   prices[classid].lastModified = current;
