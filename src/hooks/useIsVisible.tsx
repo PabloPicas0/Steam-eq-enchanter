86699c476b0ec4c7866220012b65aedb3d505b27
PabloPicas0 <pawel.fe@gmail.com>
2025-01-30 15:55:19 +0100

    refactor: useIsVisible hook supports custom root option

--- src/hooks/useIsVisible.tsx
+++ src/hooks/useIsVisible.tsx
@@ -1,19 +1,26 @@
 import { useState, useEffect } from "react";
 
-const useIsVisible = (elementRef: React.RefObject<null | HTMLElement>) => {
+const useIsVisible = (
+  elementRef: React.RefObject<null | HTMLElement>,
+  root: React.RefObject<HTMLDivElement | null>
+) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     if (elementRef.current) {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsVisible(true);
-          } else {
-            setIsVisible(false);
-          }
-        });
-      });
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              setIsVisible(true);
+            } else {
+              setIsVisible(false);
+            }
+          });
+        },
+        { root: root.current, rootMargin: "0px 100%" }
+      );
+
       observer.observe(elementRef.current);
 
       return () => {
