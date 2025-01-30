7a4fe98db7b1b26952b31eed4e3a5aec7fb610ac
PabloPicas0 <pawel.fe@gmail.com>
2025-01-30 15:49:04 +0100

    feat: coplete refactor of component for support smooth eq transition

--- src/Components/Equipment/Items.tsx
+++ src/Components/Equipment/Items.tsx
@@ -1,20 +1,56 @@
+import { useRef } from "react";
+import usePagination from "../../hooks/usePagination";
 import { EquipmentModel } from "../../models/EquipmentModel";
 import UserItem from "./UserItem";
+import UserItemList from "./UserItemList";
 
 type PropTypes = {
   filteredItems: EquipmentModel["assets"];
 };
 
+// TODO: Pagination not showing current card
 function Items(props: PropTypes) {
   const { filteredItems } = props;
 
+  const { pagination, setPagination, moveBackwards, moveForeword } = usePagination(filteredItems.length);
+  const { start, end } = pagination;
+  
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const listCount = Math.ceil(filteredItems.length / 24);
+  const lists = Array.from({ length: listCount }, (_, i) => i + 1);
+
+  const goToPrevious = () => {
+    if (carouselRef.current) carouselRef.current.scrollLeft += -carouselRef.current.offsetWidth ;
+  };
+
+  const goToNext = () => {
+    if (carouselRef.current) carouselRef.current.scrollLeft += carouselRef.current.offsetWidth ;
+  };
+
   return (
     <>
-      <ul className="items-container">
-        {filteredItems.map((item) => (
-          <UserItem key={item.market_name} item={item} isSelected={false} />
-        ))}
-      </ul>
+      <div ref={carouselRef} className="carousel-wrapper">
+        {lists.map((list) => {
+          const end = 24 * list;
+          const start = end - 24; // TODO: Missed by one
+
+          return (
+            <UserItemList key={"carousel " + list} carouselRef={carouselRef}>
+              {filteredItems.slice(start, end).map((item) => (
+                <UserItem key={item.market_name} item={item} isSelected={false} />
+              ))}
+            </UserItemList>
+          );
+        })}
+      </div>
+
+      <div className="pagination">
+        <button onClick={goToPrevious}>Back</button>
+
+        {`${start} / ${end} of ${filteredItems.length}`}
+
+        <button onClick={goToNext}>Next</button>
+      </div>
     </>
   );
 }
