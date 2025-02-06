df4c6842fd933e4acce7bd4af2f2e0b72ac2d304
PabloPicas0 <pawel.fe@gmail.com>
2025-02-06 16:21:12 +0100

    refactor: change old code to FilterSettings

--- src/Components/Equipment/Equipment.tsx
+++ src/Components/Equipment/Equipment.tsx
@@ -5,11 +5,9 @@ import useFiter from "../../hooks/useFilter";
 
 import Items from "./Items";
 import PickedItems from "./PickedItems";
-
-import FilterQuality from "./FilterQuality";
+import FilterSettings from "./FilterSettings";
 
 import { useAppSelector } from "../../hooks/useAppSelector ";
-import FilterFavourite from "./FilterFavourite";
 
 function Equipment() {
   const items = useAppSelector((state) => state.profile.items[1]);
@@ -33,45 +31,19 @@ function Equipment() {
     <section className="equipment">
       <PickedItems currencyCode={currencyCode} pickedCurrencyData={pickedCurrencyData} />
 
-      <div>
-        <h2>Total unique items: {filteredItems.length}</h2>
-
-        <div className="inputs-wrapper">
-          <input
-            className="input-steamID input-filter-equipment"
-            type="text"
-            value={nameFilter}
-            onChange={(e) => {
-              setNameFilter(e.target.value.replace("\\", ""));
-            }}
-            placeholder="Search"
-          />
-
-          <button
-            title={`Sorted ${sortAscending ? "ascending" : " descending"}`}
-            className="sort-btn"
-            onClick={() => setSortAscending((prev) => !prev)}>
-            Sort {sortAscending ? "\u2191" : "\u2193"}
-          </button>
-
-          <FilterQuality qualityFilter={qualityFilter} setQualityFilter={setQualityFilter} />
-          <FilterFavourite items={items} />
-
-          <select
-            value={currencyCode}
-            onChange={(e) => setCurrencyCode(e.target.value)}
-            className="currency-select">
-            <option value={"PLN"}>PLN</option>
-            {currenciesCodes.map((currCode) => {
-              return (
-                <option key={currCode} value={currCode}>
-                  {currCode}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-      </div>
+      <FilterSettings
+        currenciesCodes={currenciesCodes}
+        currencyCode={currencyCode}
+        items={items}
+        itemsAmmount={filteredItems.length}
+        nameFilter={nameFilter}
+        qualityFilter={qualityFilter}
+        setCurrencyCode={setCurrencyCode}
+        setNameFilter={setNameFilter}
+        setQualityFilter={setQualityFilter}
+        setSortAscending={setSortAscending}
+        sortAscending={sortAscending}
+      />
 
       <Items filteredItems={filteredItems} />
     </section>
