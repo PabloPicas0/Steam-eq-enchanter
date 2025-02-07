f95738752c3fd8f704bab4fc1a70018086c70966
PabloPicas0 <pawel.fe@gmail.com>
2025-02-07 15:00:15 +0100

    feat: create ItemsCounter component to prevent rerender all Filter settings component when only items ammount change

---
+++ src/Components/ItemsCounter.tsx
@@ -0,0 +1,8 @@
+import { useAppSelector } from "../hooks/useAppSelector ";
+
+function ItemsCounter() {
+  const itemsAmmount = useAppSelector((state) => state.profile.notPickedItems.length);
+  return <h2>Total unique items: {itemsAmmount}</h2>;
+}
+
+export default ItemsCounter;
