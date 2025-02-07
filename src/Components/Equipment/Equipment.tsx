f39c2a032ecd89128f62f1db74a3a0ff793a1c32
PabloPicas0 <pawel.fe@gmail.com>
2025-02-07 15:05:09 +0100

    perf: complete redesign of equipment component to prevent rerenders when useFiter change

--- src/Components/Equipment/Equipment.tsx
+++ src/Components/Equipment/Equipment.tsx
@@ -1,51 +1,17 @@
-import { useState } from "react";
 import "../../styles/Equipment.css";
 
-import useFiter from "../../hooks/useFilter";
-
-import Items from "./Items";
 import PickedItems from "./PickedItems";
-import FilterSettings from "./FilterSettings";
 
 import { useAppSelector } from "../../hooks/useAppSelector ";
+import NotPickedItems from "./NotPickedItems";
 
 function Equipment() {
-  const items = useAppSelector((state) => state.profile.items[1]);
   const currencies = useAppSelector((state) => state.profile.currencies);
-
-  const [currencyCode, setCurrencyCode] = useState("USD");
-  const {
-    filteredItems,
-    nameFilter,
-    sortAscending,
-    qualityFilter,
-    setNameFilter,
-    setSortAscending,
-    setQualityFilter,
-  } = useFiter(items);
-
-  const currenciesCodes = currencies[0].rates.map((rate) => rate.code);
-  const pickedCurrencyData = currencies[0].rates.find((rate) => rate.code === currencyCode);
 
   return (
     <section className="equipment">
-      <PickedItems currencyCode={currencyCode} pickedCurrencyData={pickedCurrencyData} />
-
-      <FilterSettings
-        currenciesCodes={currenciesCodes}
-        currencyCode={currencyCode}
-        items={items}
-        itemsAmmount={filteredItems.length}
-        nameFilter={nameFilter}
-        qualityFilter={qualityFilter}
-        setCurrencyCode={setCurrencyCode}
-        setNameFilter={setNameFilter}
-        setQualityFilter={setQualityFilter}
-        setSortAscending={setSortAscending}
-        sortAscending={sortAscending}
-      />
-
-      <Items filteredItems={filteredItems} />
+      <PickedItems currencies={currencies[0]} />
+      <NotPickedItems currencies={currencies[0]} />
     </section>
   );
 }
