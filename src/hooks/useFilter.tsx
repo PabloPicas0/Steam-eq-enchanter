019ff1a32b46f04e7f586f98990b2397c495cec0
PabloPicas0 <pawel.fe@gmail.com>
2025-02-07 15:11:41 +0100

    refactor: filter functionality was offladed to other thread

--- src/hooks/useFilter.tsx
+++ src/hooks/useFilter.tsx
@@ -1,22 +1,11 @@
 import { useState } from "react";
 
-import { EquipmentModel } from "../models/EquipmentModel";
-
-function useFiter(items: EquipmentModel) {
+function useFiter() {
   const [nameFilter, setNameFilter] = useState("");
   const [qualityFilter, setQualityFilter] = useState("12345678");
   const [sortAscending, setSortAscending] = useState(true);
-
-  const nameRegex = new RegExp(nameFilter, "gmi");
-  const qualityRegex = new RegExp(`[${qualityFilter}]`);
-
-  const filteredItems = items.assets
-    .filter((item) => nameRegex.test(item.name))
-    .filter((item) => qualityRegex.test(item.quality.toString()))
-    .sort((a, b) => (sortAscending ? a.quality - b.quality : b.quality - a.quality));
-
+ 
   return {
-    filteredItems,
     nameFilter,
     sortAscending,
     qualityFilter,
