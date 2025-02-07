a961d991f1656caaf45ec305b8bc3da3eab455a8
PabloPicas0 <pawel.fe@gmail.com>
2025-02-07 14:59:05 +0100

    style: delete TODO

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Mode from "./Components/Mode";
 import ScrollToTop from "./Components/ScrollToTop";
 
 // TODO: Better error handling when fetching price data
-// TODO: Functionality to reset saved price or crop ammount of prices to be saved in localstorage
 function App() {
   const currentYear = new Date().getFullYear();
   
