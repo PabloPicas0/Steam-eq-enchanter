fe5fd47544751a97c05a8246cec51446984801cf
PabloPicas0 <pawel.fe@gmail.com>
2025-02-09 16:22:28 +0100

    feat: create FilterCurrency component

---
+++ src/Components/Equipment/FilterCurrency.tsx
@@ -0,0 +1,27 @@
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { useAppSelector } from "../../hooks/useAppSelector ";
+import { newCurrencyCode } from "../../Store/Slices/profileSlice";
+
+function FilterCurrency(props: { currenciesCodes: string[] }) {
+  const { currenciesCodes } = props;
+  const currencyCode = useAppSelector((state) => state.profile.currencyCode);
+  const dispatch = useAppDispatch();
+
+  return (
+    <select
+      value={currencyCode}
+      onChange={(e) => dispatch(newCurrencyCode(e.target.value))}
+      className="currency-select">
+      <option value={"PLN"}>PLN</option>
+      {currenciesCodes.map((currCode) => {
+        return (
+          <option key={currCode} value={currCode}>
+            {currCode}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
+
+export default FilterCurrency
