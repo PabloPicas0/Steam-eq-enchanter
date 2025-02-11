9044e580da8ce803445beebe9e69425ab95095e0
PabloPicas0 <pawel.fe@gmail.com>
2025-02-11 18:09:31 +0100

    feat: create Accordion component

---
+++ src/Components/Accordion.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useState } from "react";
+
+function Accordion(props: { children: ReactNode, title: string }) {
+  const { children, title } = props;
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <div>
+      <div
+        onClick={() => setIsClicked((prev) => !prev)}
+        style={{ display: "flex", justifyContent: "space-between", userSelect: "none" }}>
+        <span>{title}</span>
+        <svg
+          className="css-q7mezt"
+          style={{ transform: isClicked ? "rotate(180deg)" : "rotate(0deg)", }}
+          focusable="false"
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          data-testid="ExpandMoreIcon">
+          <path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+        </svg>
+      </div>
+
+      <div style={{ maxHeight: isClicked ? 400 : 0, overflow: "hidden", transition: "all 250ms" }}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Accordion;
