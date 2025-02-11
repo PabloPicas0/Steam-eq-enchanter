ce566c20d26c8d69c783f8151a890c40e2f9d917
PabloPicas0 <pawel.fe@gmail.com>
2025-02-11 18:10:02 +0100

    feat: add accordion functionality and new filters

--- src/Components/Equipment/FilterQuality.tsx
+++ src/Components/Equipment/FilterQuality.tsx
@@ -1,8 +1,28 @@
-import {  useState } from "react";
+import { useState } from "react";
 
 import FilterIcon from "../../Icons/FilterIcon.svg";
 
 import "../../styles/Filter.css";
+import Accordion from "../Accordion";
+
+const itemsTypes: [string, string[]][] = [
+  [
+    "Knife",
+    "Bayonet, Bowie, Butterfly, Classic, Falchion, Flip, Gut, Huntsman, Karambit, Kukri, Bayonet, Navaja, Nomad, Paracord, Daggers, Skeleton, Stiletto, Survival, Talon, Ursus".split(
+      ","
+    ),
+  ],
+  [
+    "Pistols",
+    "CZ75 , Deagle , Berettas , Five-SeveN , Glock-18 , P2000 , P250 , Revolver , Tec-9 , USP-S".split(
+      ","
+    ),
+  ],
+  ["Heavy", "MAG-7 , Nova , Sawed-Off , XM1014, M249 , Negev".split(",")],
+  ["SMG", "MAC-10 , MP5-SD , MP7 , MP9 , P90 , PP-Bizon , UMP-45".split(",")],
+  ["Rifle", "AK-47 , AUG , FAMAS , Galil AR , M4A1-S , M4A4 , SG 553".split(",")],
+  ["Sniper", "AWP , G3SG1 , SCAR-20 , SSG 08".split(",")],
+];
 
 const itemQualityTypes = [
   ["Common", "rgb(176, 195, 217)"],
@@ -47,24 +67,45 @@ function FilterQuality(props: {
       <div
         className="filter-quality-options-wrapper"
         style={{ visibility: visibilityState, opacity: opacityState, zIndex: zIndexState }}>
-        {itemQualityTypes.map((type, idx) => {
-          const [name, color] = type;
-          const qualityType = (idx + 1).toString();
-          const isSelected = qualityFilter.includes(qualityType);
-          const handleQuality = isSelected ? deleteQuality : addQuality;
+        <Accordion title="Quality">
+          {itemQualityTypes.map((type, idx) => {
+            const [name, color] = type;
+            const qualityType = (idx + 1).toString();
+            const isSelected = qualityFilter.includes(qualityType);
+            const handleQuality = isSelected ? deleteQuality : addQuality;
 
-          return (
-            <div key={name} className="filter-quality-item" style={{ color: color }}>
-              <input
-                type="checkbox"
-                id={name}
-                checked={isSelected}
-                onChange={() => setQualityFilter((prev) => handleQuality(prev, qualityType))}
-              />
-              <label htmlFor={name}>{name}</label>
-            </div>
-          );
-        })}
+            return (
+              <div key={name} className="filter-quality-item" style={{ color: color }}>
+                <input
+                  type="checkbox"
+                  id={name}
+                  checked={isSelected}
+                  onChange={() => setQualityFilter((prev) => handleQuality(prev, qualityType))}
+                />
+                <label htmlFor={name}>{name}</label>
+              </div>
+            );
+          })}
+        </Accordion>
+
+        <Accordion title="Weapons">
+          {itemsTypes.map((type) => {
+            const [typeName, items] = type;
+
+            return (
+              <Accordion title={typeName} key={typeName}>
+                {items.map((item) => {
+                  return (
+                    <div key={item} className="filter-quality-item">
+                      <input type="checkbox" id={item} />
+                      <label htmlFor={item}>{item}</label>
+                    </div>
+                  );
+                })}
+              </Accordion>
+            );
+          })}
+        </Accordion>
       </div>
 
       {isClicked && <div className="filter-unclick-area" onClick={() => setIsClicked(false)}></div>}
