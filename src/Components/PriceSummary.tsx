c3da98f2c5a9d887b12a48a7df87956cab6db71f
PabloPicas0 <pawel.fe@gmail.com>
2025-06-06 18:30:03 +0200

    refactor: calculation and investing are no longer switchable, default state for currentAmmount is max

--- src/Components/PriceSummary.tsx
+++ src/Components/PriceSummary.tsx
@@ -6,13 +6,13 @@ type PriceSummaryProps = {
   sellProfit: number;
   buyProfit: number;
   iGetFromCurrentPrice: number;
+  ammount: number
 };
 
 function PriceSummary(props: PriceSummaryProps) {
-  const { buyProfit, iGetFromCurrentPrice, market_price, priceSuffix, sellProfit } = props;
+  const { buyProfit, iGetFromCurrentPrice, market_price, priceSuffix, sellProfit, ammount } = props;
 
-  const [isClicked, setIsClicked] = useState(false);
-  const [currentAmount, setCurrentAmount] = useState(1);
+  const [currentAmount, setCurrentAmount] = useState(ammount);
 
   const currentAmountIsNaN = Number.isNaN(currentAmount);
   const sell = currentAmountIsNaN ? "0.00" : (iGetFromCurrentPrice * currentAmount).toFixed(2);
@@ -20,54 +20,56 @@ function PriceSummary(props: PriceSummaryProps) {
   const buyColors = buyProfit > 0 ? "green" : "red";
   const sellColors = sellProfit > 0 ? "green" : "red";
 
-  return isClicked ? (
+  return (
     <>
-      <h2 className="item-price-summary-header item-price-summary" onClick={() => setIsClicked(false)}>
-        Calculations
-      </h2>
+      <div>
+        <h2 className="item-price-summary-header item-price-summary">
+          Calculations
+        </h2>
 
-      <div style={{ display: "flex", gap: 5 }}>
-        <p className="item-price-summary">Amount:</p>
-        <input
-          className="item-price-summary-input"
-          style={{ width: `${1 + currentAmount.toString().length}ch` }}
-          type="number"
-          value={currentAmount || ""}
-          onChange={(e) => {
-            const { valueAsNumber } = e.target;
+        <div style={{ display: "flex", gap: 5 }}>
+          <p className="item-price-summary">Amount:</p>
+          <input
+            className="item-price-summary-input"
+            style={{ width: `${1 + currentAmount.toString().length}ch` }}
+            type="number"
+            value={currentAmount || ""}
+            onChange={(e) => {
+              const { valueAsNumber } = e.target;
 
-            if (valueAsNumber < 1 || valueAsNumber > 10000) return;
+              if (valueAsNumber < 1 || valueAsNumber > 10000) return;
 
-            setCurrentAmount(e.target.valueAsNumber);
-          }}
-        />
+              setCurrentAmount(e.target.valueAsNumber);
+            }}
+          />
+        </div>
+
+        <p className="item-price-summary">
+          Earn from sell: {sell} {priceSuffix}
+        </p>
+
+        <p className="item-price-summary">
+          Spend for buy: {spend} {priceSuffix}
+        </p>
       </div>
+      
+      <div>
+        <h2 className="item-price-summary-header item-price-summary">
+          Investing
+        </h2>
 
-      <p className="item-price-summary">
-        Earn from sell: {sell} {priceSuffix}
-      </p>
+        <p className="item-price-summary">
+          Current price: {market_price} {priceSuffix}
+        </p>
 
-      <p className="item-price-summary">
-        Spend for buy: {spend} {priceSuffix}
-      </p>
-    </>
-  ) : (
-    <>
-      <h2 className="item-price-summary-header item-price-summary" onClick={() => setIsClicked(true)}>
-        Investing
-      </h2>
+        <p className="item-price-summary" style={{ color: sellColors }}>
+          Sell profit: {sellProfit} {priceSuffix}
+        </p>
 
-      <p className="item-price-summary">
-        Current price: {market_price} {priceSuffix}
-      </p>
-
-      <p className="item-price-summary" style={{ color: sellColors }}>
-        Sell profit: {sellProfit} {priceSuffix}
-      </p>
-
-      <p className="item-price-summary" style={{ color: buyColors }}>
-        Buy Profit: {buyProfit} {priceSuffix}
-      </p>
+        <p className="item-price-summary" style={{ color: buyColors }}>
+          Buy Profit: {buyProfit} {priceSuffix}
+        </p>
+      </div>
     </>
   );
 }
