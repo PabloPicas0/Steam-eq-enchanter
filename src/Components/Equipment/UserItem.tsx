cef96279654920b2cd2b61e7e2d9af8c1edc195b
PabloPicas0 <pawel.fe@gmail.com>
2025-06-07 16:51:59 +0200

    fix: incorrect case regex matching

--- src/Components/Equipment/UserItem.tsx
+++ src/Components/Equipment/UserItem.tsx
@@ -2,7 +2,7 @@ import "../../styles/UserItem.css";
 
 import Favourite from "./Favourite";
 
-import AmmountIcon  from "../../Icons/AmmountIcon.svg";
+import AmmountIcon from "../../Icons/AmmountIcon.svg";
 
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { addToPickedItems, removeFromPickedItems } from "../../Store/Slices/profileSlice";
@@ -13,56 +13,49 @@ type PropTypes = {
   isSelected: boolean;
 };
 
-const caseRegex = /case|capsule/gim;
+const caseRegex = /case|capsule/i;
 
 function UserItem(props: PropTypes) {
   const { item, isSelected } = props;
   const { market_name, color, name, icon_url, amount } = item;
 
   const isCase = caseRegex.test(name);
-
   const dispatch = useAppDispatch();
 
   return (
-      <div>
-        <div className="item-ammount">
-          <Favourite className="item-favourite" itemName={market_name} />
-          <img src={AmmountIcon} />
-          {amount}
+    <div>
+      <div className="item-ammount">
+        <Favourite className="item-favourite" itemName={market_name} />
+        <img src={AmmountIcon} />
+        {amount}
+      </div>
+
+      <div
+        style={{ cursor: "pointer" }}
+        onClick={() =>
+          dispatch(isSelected ? removeFromPickedItems(market_name) : addToPickedItems({ market_name, item }))
+        }
+      >
+        <div className="item-image-wrapper">
+          <img src={`https://steamcommunity-a.akamaihd.net/economy/image//${icon_url}`} className="item-image" />
         </div>
 
-        <div
-          style={{ cursor: "pointer" }}
-          onClick={() =>
-            dispatch(
-              isSelected ? removeFromPickedItems(market_name) : addToPickedItems({ market_name, item })
-            )
-          }>
-          <div className="item-image-wrapper">
-            <img
-              src={`https://steamcommunity-a.akamaihd.net/economy/image//${icon_url}`}
-              className="item-image"
-            />
-          </div>
+        <div className="item-description">
+          <p className="item-name">{name.replace(/\|.*$/g, "")}</p>
 
-          <div className="item-description">
-            <p className="item-name">{name.replace(/\|.*$/g, "")}</p>
+          {/* check if the items are cases and if yes don't display it */}
+          {isCase ? null : (
+            <p className="item-skin" style={{ color: `#${color}` }}>
+              {name.replace(/^[^\|]*\|/g, "")}
+            </p>
+          )}
 
-            {/* check if the items are cases and if yes don't display it */}
-            {isCase ? null : (
-              <p className="item-skin" style={{ color: `#${color}` }}>
-                {name.replace(/^[^\|]*\|/g, "")}
-              </p>
-            )}
-
-            {isCase ? null : (
-              <p className="item-category">
-                {market_name.replace(/^[^()]*()/g, "").replace(/[\\(\\)]/g, "")}
-              </p>
-            )}
-          </div>
+          {isCase ? null : (
+            <p className="item-category">{market_name.replace(/^[^()]*()/g, "").replace(/[\\(\\)]/g, "")}</p>
+          )}
         </div>
       </div>
+    </div>
   );
 }
 
