87f1a989194cea3a77cbda169cb8b315307c3399
PabloPicas0 <pawel.fe@gmail.com>
2025-06-08 16:54:20 +0200

    refactor: change type for workerScript param

--- src/hooks/useWebWorker.tsx
+++ src/hooks/useWebWorker.tsx
@@ -1,11 +1,11 @@
 import { useState, useRef, useEffect } from "react";
 
-function useWebWorker<Task, Return>(workerScript: URL, defaultState: Return) {
+function useWebWorker<Task, Return>(workerScript: Worker, defaultState: Return) {
   const [result, setResult] = useState<Return>(defaultState);
   const workerRef = useRef<Worker>(null);
 
   useEffect(() => {
-    workerRef.current = new Worker(workerScript);
+    workerRef.current = workerScript;
 
     workerRef.current.onmessage = (event: MessageEvent<Return>) => {
       setResult(event.data);
